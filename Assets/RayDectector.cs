5b983cc328919e3646e2bda898a63a8430ea4ee9
Torcu <45042365+Exater5@users.noreply.github.com>
2022-01-27 16:30:55 +0100

    Blocking Ray

---
+++ Assets/RayDectector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayDectector : MonoBehaviour
+{
+    [SerializeField] LineRenderer _lRenderer;
+    [SerializeField] LayerMask _mask;
+    [SerializeField] int _posIndex;
+    Vector3 initialPoint;
+    bool detected;
+
+    private void Start()
+    {
+        initialPoint = _lRenderer.GetPosition(_posIndex);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position,transform.forward, out hit, Mathf.Infinity, _mask))
+        {
+            _lRenderer.SetPosition(_posIndex, hit.point);
+            detected = true;
+        }
+        else if(detected)
+        {
+            _lRenderer.SetPosition(_posIndex, initialPoint);
+            detected = false;
+        }
+    }
+}
