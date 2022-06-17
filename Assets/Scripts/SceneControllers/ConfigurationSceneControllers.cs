61c81c768652b02014ad51408f0ca54177df22ab
Torcu <45042365+Exater5@users.noreply.github.com>
2022-06-17 16:46:20 +0200

    Configuration Splash Images

--- Assets/Scripts/SceneControllers/ConfigurationSceneControllers.cs
+++ Assets/Scripts/SceneControllers/ConfigurationSceneControllers.cs
@@ -7,6 +7,7 @@ public class ConfigurationSceneControllers : MonoBehaviour
 
     [SerializeField] GameObject _logosHolder1;
     [SerializeField] GameObject _logosHolder2;
+    [SerializeField] GameObject _logosHolder3;
 
     private void Awake()
     {
@@ -25,6 +26,11 @@ public class ConfigurationSceneControllers : MonoBehaviour
         _logosHolder1.SetActive(false);
         yield return new WaitForSeconds(1f);
         _logosHolder2.SetActive(true);
+        yield return FindObjectOfType<TransitionsController>().coFadeFromBlack(2f);
+        yield return new WaitForSeconds(1.5f);
+        yield return FindObjectOfType<TransitionsController>().coFadeToBlack(2f);
+        _logosHolder2.SetActive(false);
+        _logosHolder3.SetActive(true);
         yield return FindObjectOfType<TransitionsController>().coFadeFromBlack(2f);
         yield return new WaitForSeconds(3f);
         GameEvents.LoadScene.Invoke("MainMenu");
